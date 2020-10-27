beda9334b4d98c38682662ca2c6c0e449d4dada4
Alexander Klimetschek <aklimets@adobe.com>
2020-10-26 21:04:47 -0700

    Revert "detect main = main case #39"
    This reverts commit 7e31aa1b5e3aee6e8577a82ddffcdd5f67f3ff66.

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ async function main(argv) {
             throw new Error(`Not inside the root of a git checkout: ${process.cwd()}`);
         }
 
+        // TODO: detect main = main case
+
         console.log(`Cloning git repository...`);
         const { before, after } = await gitCheckoutBeforeAndAfter(process.cwd(), argv[0], argv[1]);
 
